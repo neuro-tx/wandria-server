b0c76e3eb49501d7efe30b96c8d1a7d83b6968ac
tarek fawzy <tf5234045@gmail.com>
2025-07-04 06:28:02 +0300

    fix: fix data date for charts

--- utils/latest.js
+++ utils/latest.js
@@ -8,13 +8,13 @@ const getWeeklySummary = async (Model, dateField = "createdAt") => {
   endOfWeek.setHours(0, 0, 0, 0);
 
   const dayMap = {
-    0: "Sunday",
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday",
   };
 
   const allDays = Object.values(dayMap).map((day) => ({
